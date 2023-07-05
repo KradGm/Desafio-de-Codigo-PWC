02656587f118a9ecbf2d4e7e428328acf9ee6a04
KradGm <enzo.loliveira@hotmail.com>
2023-07-05 13:21:31 -0300

    Adicionar arquivos de projeto.

---
+++ Desafio de Codigo PWC/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string texto1 = "Hello, World! OpenAi is amazing.";
+string[] texto1Array = texto1.Split(" ");
+
+Console.Write(texto1Array[4] + " " + texto1Array[3] + " " + texto1Array[2] + " " + texto1Array[1] + " " + texto1Array[0]);
+Console.ReadLine();
