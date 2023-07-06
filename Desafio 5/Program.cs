c47c8fec3767cf39816ecab35380fabb19536d23
KradGm <enzo.loliveira@hotmail.com>
2023-07-06 14:49:08 -0300

    Desafio 5

---
+++ Desafio 5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static string ReverseString(string input)
+    {
+        char[] characters = input.ToCharArray();
+        Array.Reverse(characters);
+        return new string(characters);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite uma frase:");
+        string input = Console.ReadLine();
+
+        string reversedString = ReverseString(input);
+        Console.WriteLine("Frase invertida: " + reversedString);
+
+        if (input.Replace(" ", "").ToLower() == reversedString.Replace(" ", "").ToLower())
+        {
+            Console.WriteLine("True");
+        }
+        else
+        {
+            Console.WriteLine("False");
+        }
+    }
+
+}
